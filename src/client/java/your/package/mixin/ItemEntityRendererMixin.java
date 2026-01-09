06f8a600e8ffb71300f33baad643ed6fc5d54a49
v3xa00 <tomekgregori777@gmail.com>
2026-01-09 08:58:16 +0100

    Create ItemEntityRendererMixin.java

---
+++ src/client/java/your/package/mixin/ItemEntityRendererMixin.java
@@ -0,0 +1,38 @@
+package your.package.mixin;
+
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.ItemEntityRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.ItemEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import your.package.ScaleNbt;
+
+@Mixin(ItemEntityRenderer.class)
+public class ItemEntityRendererMixin {
+
+    @Inject(method = "render(Lnet/minecraft/entity/ItemEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V",
+            at = @At("HEAD"))
+    private void rozszerzitem$pushAndScale(ItemEntity entity, float yaw, float tickDelta,
+                                          MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light,
+                                          CallbackInfo ci) {
+        float scale = ScaleNbt.getScale(entity.getStack());
+        if (scale != 1.0f) {
+            matrices.push();
+            matrices.scale(scale, scale, scale);
+        }
+    }
+
+    @Inject(method = "render(Lnet/minecraft/entity/ItemEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V",
+            at = @At("TAIL"))
+    private void rozszerzitem$pop(ItemEntity entity, float yaw, float tickDelta,
+                                 MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light,
+                                 CallbackInfo ci) {
+        float scale = ScaleNbt.getScale(entity.getStack());
+        if (scale != 1.0f) {
+            matrices.pop();
+        }
+    }
+}
