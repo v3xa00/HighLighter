7bd6a5d9a687e297b64ade76a0da09ac7988b557
v3xa00 <tomekgregori777@gmail.com>
2026-01-09 08:57:22 +0100

    Create RozszerzItemMod.java

---
+++ src/main/java/your/package/RozszerzItemMod.java
@@ -0,0 +1,43 @@
+package your.package;
+
+import com.mojang.brigadier.arguments.DoubleArgumentType;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+
+public class RozszerzItemMod implements ModInitializer {
+
+    public static final String MOD_ID = "rozszerzitem";
+
+    @Override
+    public void onInitialize() {
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            dispatcher.register(
+                CommandManager.literal("rozszerz")
+                    .then(CommandManager.argument("skala", DoubleArgumentType.doubleArg(0.1D, 16.0D))
+                        .executes(ctx -> {
+                            ServerPlayerEntity player = ctx.getSource().getPlayerOrThrow();
+                            double scaleD = DoubleArgumentType.getDouble(ctx, "skala");
+                            float scale = (float) scaleD;
+
+                            ItemStack held = player.getMainHandStack();
+                            if (held.isEmpty()) {
+                                ctx.getSource().sendFeedback(() -> Text.literal("Nie trzymasz żadnego itemu w głównej ręce."), false);
+                                return 0;
+                            }
+
+                            ScaleNbt.setScale(held, scale);
+                            ctx.getSource().sendFeedback(
+                                () -> Text.literal("Ustawiono skalę " + scale + " dla: " + held.getName().getString()),
+                                false
+                            );
+                            return 1;
+                        })
+                    )
+            );
+        });
+    }
+}
